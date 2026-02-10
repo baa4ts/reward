762ef7a70f1331c4e2702185bf89b66b8fb53175
baa4ts <baa4tsdev@gmail..com>
2026-02-10 18:57:15 -0300

    Update

--- src/reward/components/Hero.tsx
+++ src/reward/components/Hero.tsx
@@ -1,14 +1,14 @@
-import React from "react"
+import React from "react";
 
 export const Hero = React.memo(() => {
     return (
         <section className="custom-width flex flex-col-reverse gap-2 py-10 text-center lg:flex-row">
-            <article className="flex flex-1 flex-col justify-center gap-4 text-left">
-                <h1 className="px-2 font-mono text-5xl font-bold leading-tight">
+            <article className="flex text-center items-center flex-1 flex-col justify-center gap-4">
+                <h1 className="w-full px-2 font-mono text-5xl font-bold leading-tight">
                     Reward
                 </h1>
 
-                <p className="max-w-md px-2 text-xl text-gray-400">
+                <p className="w-full max-w-md px-2 text-xl text-gray-400">
                     Reclama tus recompensas de{" "}
                     <span className="font-semibold text-yellow-400">
                         Clash Royale
@@ -18,7 +18,7 @@ export const Hero = React.memo(() => {
 
                 <div className="mt-2 flex items-center justify-center md:flex-none">
                     <a href="#reward-now">
-                        <button className="custom-scale rounded-lg border-2 border-black bg-yellow-400 px-6 py-3 font-mono font-extrabold text-black  hover:bg-yellow-300">
+                        <button className="custom-scale rounded-lg border-2 border-black bg-yellow-400 px-6 py-3 font-mono font-extrabold text-black hover:bg-yellow-300">
                             Reclamar ahora
                         </button>
                     </a>
@@ -33,5 +33,5 @@ export const Hero = React.memo(() => {
                 />
             </article>
         </section>
-    )
-})
+    );
+});
